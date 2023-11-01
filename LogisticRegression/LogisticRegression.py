1d720116de6abaf64dda1c913ec944235820a12c
Vishnupriya Varadharaju <35620007+see-k-er@users.noreply.github.com>
2023-11-01 13:22:03 -0400

    Logistic Regression

---
+++ LogisticRegression/LogisticRegression.py
@@ -0,0 +1,37 @@
+#Logistic Regression implementation from scratch
+
+
+import numpy as np
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+class LogisticRegression():
+
+    def __init__(self, lr=0.01, n_iters=1000):
+        self.lr = lr
+        self.n_iters = n_iters
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.n_iters):
+            linear_pred = np.dot(X, self.weights) + self.bias
+            predictions = sigmoid(linear_pred)
+
+            dw = (1/n_samples) * 2 * np.dot(X.T, (predictions-y))
+            db = (1/n_samples) * 2 * np.sum(predictions - y)
+
+            self.weights = self.weights - (self.lr*dw)
+            self.bias = self.bias - (self.lr*db)
+
+    def predict(self, X):
+        linear_pred = np.dot(X, self.weights) + self.bias
+        y_pred = sigmoid(linear_pred)
+        #applying the thresholds
+        class_pred = [0 if y <= 0.5 else 1 for y in y_pred]
+        return class_pred
